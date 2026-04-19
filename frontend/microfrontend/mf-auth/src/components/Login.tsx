ab07727f097a2e84395baabfefefb52ec7f02517
Kirill <marchenko.kiri11@yandex.ru>
2026-04-19 18:24:14 +0300

    fix: need update logic for build project

---
+++ frontend/microfrontend/mf-auth/src/components/Login.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+interface LoginProps {
+  onLogin: (userData: { email: string; password: string }) => void;
+}
+
+function Login({ onLogin }: LoginProps) {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const userData = {
+      email,
+      password,
+    };
+    onLogin(userData);
+  }
+  return (
+    <div className="auth-form">
+      <form className="auth-form__form" onSubmit={handleSubmit}>
+        <div className="auth-form__wrapper">
+          <h3 className="auth-form__title">Вход</h3>
+          <label className="auth-form__input">
+            <input
+              type="text"
+              name="name"
+              id="email"
+              className="auth-form__textfield"
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </label>
+          <label className="auth-form__input">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="auth-form__textfield"
+              placeholder="Пароль"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </label>
+        </div>
+        <button className="auth-form__button" type="submit">
+          Войти
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
